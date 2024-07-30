d08adb7654b62422e5c520e70187d6857fb68e87
jyunseon81 <102278574+jyunseon81@users.noreply.github.com>
2024-07-30 18:38:12 +0900

    Feat: Freddy Spawn, Remove 구현

--- Source/Pizza/Private/JYS/EnemyFreddy.cpp
+++ Source/Pizza/Private/JYS/EnemyFreddy.cpp
@@ -2,12 +2,17 @@
 
 
 #include "JYS/EnemyFreddy.h"
+#include "HJS/FreddyPlayer.h"
+#include "EngineUtils.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 AEnemyFreddy::AEnemyFreddy()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
 
 }
 
@@ -17,6 +22,16 @@ void AEnemyFreddy::BeginPlay()
 	Super::BeginPlay();
 	
 	SetAILevel(3);
+
+	// 큐브(Freddy) 스폰 타이머
+	GetWorld()->GetTimerManager().SetTimer(CubeSpawnTimerHandle, this, &AEnemyFreddy::AttemptSpawnCube, 3.02f, true);
+
+	// Player 객체 찾기
+	for (TActorIterator<AFreddyPlayer> It(GetWorld()); It; ++It)
+	{
+		Player = *It;
+		break;
+	}
 }
 
 // Called every frame
@@ -24,5 +39,119 @@ void AEnemyFreddy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AEnemyFreddy::AttemptSpawnCube()
+{
+	// 만약 플레이어가 침대를 바라보고 있지 않다면
+	if (!IsPlayerLookingAtBed())
+	{
+		// 랜덤 넘버를 고른다
+		int32 RandomNumber = GetRandomNumber();
+
+		// 랜덤 넘버가 Freddy의 레벨보다 낮거나 같다면
+		if (RandomNumber <= Level)
+		{
+			// 큐브(Freddy) 스폰 시작
+			FActorSpawnParameters SpawnParams;
+			SpawnParams.Owner = this;
+			SpawnParams.Instigator = GetInstigator();
+
+			FVector SpawnLocation = GetActorLocation();
+			switch (SpawnedCubes.Num())
+			{
+			// Middle Freddy
+			case 0: 
+				SpawnLocation += FVector(0, 0, 50);
+				break;
+			// Right Freddy
+			case 1:
+				SpawnLocation += FVector(50, 50, 50);
+				break;
+			// Left Freddy
+			case 2:
+				SpawnLocation += FVector(-50, 50, 50);
+				break;
+			}
+
+			FRotator SpawnRotation = FRotator::ZeroRotator;
+			AActor* SpawnedCube = GetWorld()->SpawnActor<AActor>(AActor::StaticClass(), SpawnLocation, SpawnRotation, SpawnParams);
+
+			if (SpawnedCube)
+			{
+				UStaticMeshComponent* MeshComp = NewObject<UStaticMeshComponent>(SpawnedCube);
+				MeshComp->SetStaticMesh(ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("/Script/Engine.SkeletalMesh'/Engine/EngineMeshes/SkeletalCube.SkeletalCube'")).Object);
+				MeshComp->SetupAttachment(SpawnedCube->GetRootComponent());
+				SpawnedCube->SetRootComponent(MeshComp);
+				SpawnedCubes.Add(SpawnedCube);
+			}
+		}
+	}
+}
+
+void AEnemyFreddy::ShrinkAndRemoveCubes()
+{
+	// 큐브 스폰 순서
+	if (ShrinkIndex < SpawnedCubes.Num())
+	{
+		AActor* CubeToShrink = nullptr;
+		switch (SpawnedCubes.Num())
+		{
+			case 3:
+				CubeToShrink = (ShrinkIndex == 0) ? SpawnedCubes[2] : (ShrinkIndex == 1) ? SpawnedCubes[1] :SpawnedCubes[0];
+				break;
+			case 2:
+				CubeToShrink = (ShrinkIndex == 0) ? SpawnedCubes[1] : SpawnedCubes[0];
+				break;
+			case 1:
+				CubeToShrink = SpawnedCubes[0];
+				break;
+		}
+		if (CubeToShrink)
+		{
+			UStaticMeshComponent* MeshComp = CubeToShrink->FindComponentByClass<UStaticMeshComponent>();
+			if (MeshComp)
+			{
+				FVector NewScale = MeshComp->GetComponentScale() * 0.9f;
+				MeshComp->SetWorldScale3D(NewScale);
+
+				// 만약 큐브의 Scale이 0.1f 이하라면 Destroy
+				if (NewScale.GetMin() < 0.1f)
+				{
+					SpawnedCubes.Remove(CubeToShrink);
+					CubeToShrink->Destroy();
+					ShrinkIndex++;
+					// 0.5초마다 큐브 remove 시작
+					GetWorld()->GetTimerManager().SetTimer(ShrinkTimerHandle, this, &AEnemyFreddy::ShrinkAndRemoveCubes, 0.5f, false);
+				}
+			}
+		}
+	}
+	else
+	{
+		// 모든 과정이 끝난 후 Index 초기화
+		ShrinkIndex = 0;
+		GetWorld()->GetTimerManager().ClearTimer(ShrinkTimerHandle);
+	}
+}
+
+int32 AEnemyFreddy::GetRandomNumber()
+{
+	return FMath::RandRange(1,20);
+}
+
+bool AEnemyFreddy::IsPlayerLookingAtBed()
+{
+	if (Player)
+	{
+		return Player->GetLookAtState() == AFreddyPlayer::LookAt::Bed;
+	}
+	return false;
+}
+
+void AEnemyFreddy::StartShrinkingCubes()
+{
+	ShrinkIndex = 0;
+	ShrinkAndRemoveCubes();
 }
 

1c7f1a721bbf777e2d545cc251ca686144cffce5
jyunseon81 <102278574+jyunseon81@users.noreply.github.com>
2024-08-09 18:41:27 +0900

    Feat: Add Freddy Jumpscare Animation

---
+++ Source/Pizza/Private/JYS/FreddyAnimInstance.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JYS/FreddyAnimInstance.h"
+#include "JYS/EnemyFreddy.h"
+
+void UFreddyAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	Freddy = GetOwningComponent();
+}
+
+void UFreddyAnimInstance::FreddyJumpscareAnimation()
+{
+	//UE_LOG(LogTemp,Warning,TEXT("1111111"));
+	//AEnemyFreddy* Me = Cast<AEnemyFreddy>(TryGetPawnOwner());
+	Montage_Play(FreddyJumpscareMontage);
+	//Me->GetMesh()->GetAnimInstance()->Montage_Play(FreddyJumpscareMontage);
+}
